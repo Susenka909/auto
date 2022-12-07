6034f0a4ac1cba94ba786d0e292fc6e712c189d2
Susenka909 <116568733+Susenka909@users.noreply.github.com>
2022-12-07 18:11:29 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,42 @@
+function 右转 () {
+    wuKong.setAllMotor(-80, 0)
+}
+function 左转 () {
+    wuKong.setAllMotor(0, -80)
+}
+function 前进 () {
+    wuKong.setAllMotor(-100, -100)
+}
+basic.showIcon(IconNames.House)
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 0) {
+        前进()
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 1) {
+        右转()
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . . . .
+            `)
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P2) == 0) {
+        左转()
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . . . .
+            `)
+    }
+})
